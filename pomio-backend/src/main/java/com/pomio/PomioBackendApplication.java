1f90b991d8b78c20ddd09e2c70e5ed2a54282e01
Anthony Clement <antclem6@gmail.com>
2025-05-27 17:52:27 -0400

    backend initalized

---
+++ pomio-backend/src/main/java/com/pomio/PomioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pomio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomioBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomioBackendApplication.class, args);
+    }
+
+}
---
+++ pomio-backend/src/test/java/com/pomio/PomioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
